047cc332f1abf4e3f33236906cab7efd2297a5cd
Jerry Li <jerrylijiaming@gmail.com>
2017-03-12 17:58:15 +1100

    Added file to run the clustering function

---
+++ run_clustering.py
@@ -0,0 +1,19 @@
+import data_util
+import argparse
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_dir", required=True, help="path to folder containing images")
+    parser.add_argument("--db_dir", default = "pixiv_1T_db.pkl",
+                        help="path to an already existing database or to store the new database.")
+    parser.add_argument("--output_dir", default = "pixiv_1T_clustered/", help="output path")
+    parser.add_argument("--tag_max_count", type=int, default=10000,
+                        help="number of the most popular tags to be taken into account.")
+    parser.add_argument("--num_clusters", type=int, default=20, help="Number of clusters to generate.")
+    a = parser.parse_args()
+    data_util.cluster_images(a.input_dir, a.db_dir, a.output_dir, a.tag_max_count, a.num_clusters)
+
+"""
+python run_clustering.py --input_dir=/mnt/pixiv_drive/home/ubuntu/PycharmProjects/PixivUtil2/pixiv_downloaded/
+"""
